8e15e20c3f7dd3cd8df59481605a95594cf6ce47
Emily Palmer <emilymmpalmer@gmail.com>
2025-02-12 14:41:54 -0800

    added initial man files

--- R/helpers.R
+++ R/helpers.R
@@ -1,14 +1,48 @@
 # Various -----------------------------------------------------------------
-get_Yij0 <- function(i, j, Y){
+#' Get $Y_{ij0}$
+#'
+#' @param i subject
+#' @param j time index
+#' @param Y response
+#' @param mi number of time points for subject i
+#'
+#' @returns
+#'
+#' @examples
+get_Yij0 <- function(i, j, Y, mi){
   Yij0 <- sum(Y[((i - 1)*mi + j), ])
   return(Yij0)
 }
 
-get_Bij <- function(i, j, B){
+#' Get $B_ij$
+#'
+#' @param i subject
+#' @param j time index
+#' @param B B-spline basis matrix
+#'
+#' @returns
+#'
+#' @examples
+get_Bij <- function(i, j, B, mi){
   Bij <- B[((i - 1)*mi + j), ]
   return(Bij)
 }
 
+
+
+
+
+#' Get $Z_{ijl}$
+#'
+#' @param i subject
+#' @param j time index
+#' @param l external variable index
+#' @param Z matrix of external variables
+#'
+#' @returns
+#' @export
+#'
+#' @examples
 get_Z_ijl <- function(i, j, l, Z){
   Z_ijl <- Z[i + (j - 1), (l + 1)]
   return(Z_ijl)
