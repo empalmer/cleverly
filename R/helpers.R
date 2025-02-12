56b0bb42a1884137514669b85c6dcc66cda48598
Emily Palmer <emilymmpalmer@gmail.com>
2025-02-12 15:09:02 -0800

    styled

--- R/helpers.R
+++ R/helpers.R
@@ -9,8 +9,8 @@
 #' @returns
 #'
 #' @examples
-get_Yij0 <- function(i, j, Y, mi){
-  Yij0 <- sum(Y[((i - 1)*mi + j), ])
+get_Yij0 <- function(i, j, Y, mi) {
+  Yij0 <- sum(Y[((i - 1) * mi + j), ])
   return(Yij0)
 }
 
@@ -26,8 +26,8 @@ get_Yij0 <- function(i, j, Y, mi){
 #' @returns
 #'
 #' @examples
-get_Bij <- function(i, j, B, mi){
-  Bij <- B[((i - 1)*mi + j), ]
+get_Bij <- function(i, j, B, mi) {
+  Bij <- B[((i - 1) * mi + j), ]
   return(Bij)
 }
 
@@ -45,7 +45,7 @@ get_Bij <- function(i, j, B, mi){
 #' @returns
 #'
 #' @examples
-get_Z_ijl <- function(i, j, l, Z){
+get_Z_ijl <- function(i, j, l, Z) {
   Z_ijl <- Z[i + (j - 1), (l + 1)]
   return(Z_ijl)
 }
